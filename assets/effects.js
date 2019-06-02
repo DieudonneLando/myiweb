ac9ed5e37b68eff0dc3541c7e0183f6564531c44
CaptainDieudo <dieudonne@captaintower.com>
2019-06-02 18:01:21 +0200

    update

--- assets/effects.js
+++ assets/effects.js
@@ -20,15 +20,26 @@ $(document).ready(function () {
             s = e
         }
     }), s.forEach(function (e) {
+        if (e.name.indexOf('native') != -1) {
+            $(".native-centered").append("<div class='col-lg-2 native-cube'><strong>" + e.name + "</strong>" + e.description + "<br>" + e.language + "<br><br>" + new Date(e.created_at).toLocaleDateString("fr") + "</div>");
+        }
         $(".row-centered").append("<div class='col-lg-2 cube'><strong>" + e.name + "</strong>" + e.description + "<br>" + e.language + "<br><br>" + new Date(e.created_at).toLocaleDateString("fr") + "</div>");
     });
-    $(".cube").click(function (e) {
+    $(".cube , .native-cube").click(function (e) {
         let nameDiv = $.parseHTML(e.currentTarget.innerHTML);
         s.forEach(function (e) {
             if (nameDiv[0].innerHTML == e.name) {
                 e.name == 'WooSowMe' ? window.open('https://woosowmeapp.ga/authentification', '_blank') : window.open(e.html_url, '_blank');
             }
         });
-
+    });
+    $(".avatar").click(function (e) {
+        window.open('https://github.com/DDieudonne', '_blank')
+    });
+    $(".ionic-class").click(function (e) {
+        window.open('tuto.html', '_self');
+    });
+    $(".all-repo-class").click(function (e) {
+        window.open('repositories.html', '_self');
     });
 });
